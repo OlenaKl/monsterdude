ba88ae0b1cd3b57216d55cec8b4cbee9b8249867
Lena Kulichenko <lena.kulichenko@mail.ru>
2018-08-17 17:06:46 +0300

    Resonsive

--- monster/js/script.js
+++ monster/js/script.js
@@ -1,10 +1,35 @@
-$('.slider').slick({
-  infinite: true,
-  slidesToShow: 3,
-  slidesToScroll: 3
-});
+(function slider(){
+  if (document.documentElement.clientWidth < 400){
+    $('.slider').slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      centerMode: true,
+      variableWidth: true
+    });
+    $('.testimonials-section_slider').slick({
+      infinite: true,
+      dots: true,
+      centerMode: true
+    });	
+  } else {
+    $('.slider').slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 3
+    });
+    $('.testimonials-section_slider').slick({
+      infinite: true,
+      dots: true
+    });	
+  }
+})();
 
-$('.testimonials-section_slider').slick({
-  infinite: true,
-  dots: true
-});	
+
+
+
+
+$('.hamburger').click(function(){
+  $(this).toggleClass('active');
+  $('.header-menu').toggleClass('show-mobile');
+});
